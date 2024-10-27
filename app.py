df3f0cdd76c6eae64bb0cdbc34abed9d68e12e9a
MJK <mj@jcc-hamburg.com>
2024-10-27 21:23:07 +0100

    Automated commit at 241027212306

--- app.py
+++ app.py
@@ -23,6 +23,7 @@ class CalendarFeed(db.Model):
     name = db.Column(db.String(100), nullable=False)  # Custom name for the feed
     url = db.Column(db.String(500), unique=True, nullable=False)
     events = db.relationship('Event', backref='feed', cascade="all, delete", lazy=True)
+    
 
 
 # Model to store events from calendar feeds
