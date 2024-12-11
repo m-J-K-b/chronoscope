2c880c26d7968f9e5df12e575f5f46bb013659aa
m-J-K-b <mj@jcc-hamburg.com>
2024-12-11 13:57:22 +0100

    Automated commit at 241211135721

--- app/event_bp/forms.py
+++ app/event_bp/forms.py
@@ -1,5 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import DateField, IntegerField, SelectField, StringField, SubmitField
+from wtforms.fields import (
+    DateField,
+    IntegerField,
+    SelectField,
+    StringField,
+    SubmitField,
+    TextAreaField,
+)
 from wtforms.validators import DataRequired, Optional
 
 
@@ -7,7 +14,7 @@ class EventCreationForm(FlaskForm):
     name = StringField("Event Name", validators=[DataRequired()])
     start_date = DateField("Start Date", validators=[DataRequired()])
     end_date = DateField("End Date", validators=[Optional()])
-    description = StringField("Description", validators=[Optional()])
+    description = TextAreaField("Description", validators=[Optional()])
     calendar_feed_id = SelectField(
         "Calendar Feed", coerce=int, validators=[DataRequired()]
     )
